8c003d590a012528219471991577be49f50646b4
Qwerti7777 <ivanstukalev0@gmail.com>
2022-12-04 21:38:16 +0300

    btn clik me

--- main.py
+++ main.py
@@ -7,4 +7,23 @@ label.grid()
 
 print("hello")
 
+count = {
+    "i":1
+}
+
+def fun():
+    if count["i"] %2 == 1:
+        print("меня нажали 1 раз")
+    else:
+        print("меня нажали 2 раз")
+    count["i"] = count["i"] + 1  
+
+def fun1():
+    fun()
+
+
+btn = tk.Button(root, text="click me", command=fun1)
+btn.grid()
+
+
 root.mainloop()
